89fd5a5df1d9f80edac9733e791a02ee6f3db1aa
Braxton McClellan <thebraxmc@gmail.com>
2021-02-13 23:22:34 -0700

    added services to home

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,20 +1,73 @@
 import React from 'react' 
 
-import { HomeWrap, HeaderWrap, HeaderCard, Header, Ul, Info } from './HomeElements'
+import { HomeWrap, HeaderWrap, HeaderCard, Header, Ul, Info, ServicesWrap, Regular, Img, ServiceCard, ServiceTitle, ServiceInfo, InfoWrap, Flip } from './HomeElements'
+
+import Service1 from '../images/sample2.jpg'
+import Service2 from '../images/sample1.jpg'
+import Service3 from '../images/sample3.png'
+import Service4 from '../images/sample4.jpg'
 
 const Home = () => (
-  <HomeWrap>
-    <HeaderWrap>
-      <HeaderCard>
-        <Header>Utah's Best Cleaning Service, and we're here to prove it!</Header>
-        <Ul>
-        <Info>We are an eco-friendly cleaning service!</Info>
-        <Info>We use products that are not only good for your home, but for the planet as well!</Info>
-        <Info>We offer cleaning services for residential homes, businesses, and post-construction homes.</Info>
-        </Ul>
-      </HeaderCard>
-    </HeaderWrap>
-  </HomeWrap>
+  <>
+    <HomeWrap>
+      <HeaderWrap>
+        <HeaderCard>
+          <Header>Utah's Best Cleaning Service, and we're here to prove it!</Header>
+          <Ul>
+          <Info>We are an eco-friendly cleaning service!</Info>
+          <Info>We use products that are not only good for your home, but for the planet as well!</Info>
+          <Info>We offer cleaning services for residential homes, businesses, and post-construction homes.</Info>
+          </Ul>
+        </HeaderCard>
+      </HeaderWrap>
+    </HomeWrap>
+    <ServicesWrap>
+      <Regular>
+        <Img src={Service1} alt='Picture of residential cleaning' />
+        <InfoWrap>
+          <ServiceCard>
+            <ServiceTitle>Residential Cleaning</ServiceTitle>
+            <ServiceInfo>Sample text</ServiceInfo>
+            <ServiceInfo>Sample text</ServiceInfo>
+            <ServiceInfo>Sample text</ServiceInfo>
+          </ServiceCard>
+        </InfoWrap>
+      </Regular>
+      <Flip>
+        <Img src={Service2} alt='Picture of business cleaning' />
+        <InfoWrap>
+          <ServiceCard>
+            <ServiceTitle>Business Cleaning</ServiceTitle>
+            <ServiceInfo>Sample text</ServiceInfo>
+            <ServiceInfo>Sample text</ServiceInfo>
+            <ServiceInfo>Sample text</ServiceInfo>
+          </ServiceCard>
+        </InfoWrap>
+      </Flip>
+      <Regular>
+        <Img src={Service3} alt='Picture of one time cleaning' />
+        <InfoWrap>
+          <ServiceCard>
+            <ServiceTitle>One Time Cleaning</ServiceTitle>
+            <ServiceInfo>Sample text</ServiceInfo>
+            <ServiceInfo>Sample text</ServiceInfo>
+            <ServiceInfo>Sample text</ServiceInfo>
+          </ServiceCard>
+        </InfoWrap>
+      </Regular>
+      <Flip>
+        <Img src={Service4} alt='Picture of post construction cleaning' />
+        <InfoWrap>
+          <ServiceCard>
+            <ServiceTitle>Post-Construction Cleaning</ServiceTitle>
+            <ServiceInfo>Sample text</ServiceInfo>
+            <ServiceInfo>Sample text</ServiceInfo>
+            <ServiceInfo>Sample text</ServiceInfo>
+          </ServiceCard>
+        </InfoWrap>
+      </Flip>
+    </ServicesWrap>
+  </>
 )
 
 export default Home;
--- client/src/components/home/HomeElements.js
+++ client/src/components/home/HomeElements.js
@@ -46,3 +46,61 @@ export const Info = styled.li`
   width: 600px;
 `
 
+export const ServicesWrap = styled.div`
+
+`
+
+export const Regular = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  background-color: #019DD0;
+  height: 450px;
+`
+
+export const Flip = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: space-evenly;
+  background-color: #60BC46;
+  height: 450px;
+`
+
+export const Img = styled.img`
+  display: flex;
+  align-self: center;
+  border-radius: 20px;
+  border: 2px solid #000;
+  width: 50%;
+  height: 380px;
+  width: 380px;
+`
+
+export const InfoWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 50vw;
+`
+
+export const ServiceCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 20px;
+  border: 2px solid #000;
+  width: 40vw;
+  height: 30vh;
+`
+
+export const ServiceTitle = styled.h2`
+  padding-top: 20px;
+  font-family: 'Oswald', sans-serif;
+  font-size: 35px;
+`
+
+export const ServiceInfo = styled.li`
+  padding-top: 20px;
+  font-family: 'Montserrat', sans-serif;
+`
