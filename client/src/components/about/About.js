af4a5ff6f905cf3a9a38cdce45df8d91b080058e
Braxton McClellan <thebraxmc@gmail.com>
2021-02-17 23:54:43 -0700

    about page

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
-import { AboutWrap } from './AboutElements'
+import { AboutWrap, AboutCard, AboutTitle, AboutInfo } from './AboutElements'
 
 const About = () => (
   <AboutWrap>
-  
+      <AboutTitle>About Us</AboutTitle>
+    <AboutCard>
+      <AboutInfo>Our names are Grayson and Taylor. Clean & Green is our eco-friendly cleaning business that we just launched in January of 2021. We are Salt Lake Valley natives, and we cannot wait to service our neighbors throughout the valley. Clean & Green was created with the thought of being able to share some of what’s important to us in our lives with our customers; namely our peace of mind while living/working in a clean space and our love for the planet.</AboutInfo>
+      <AboutInfo>It’s easy to get caught up in the chaos of living life. We all have a lot on our plates to handle at once and that creates stress. By taking care of everything in our personal lives, it is sometimes easy to push cleaning to the side. However, we believe that having a clean space to live or work in is an easy way to alleviate some stress. In our personal lives, we love to have our living space (which for the past year has also been our working space) clean. The breath of relief that is felt right when the whole area is fresh is so wonderful. We feel that this is an excellent source of achieving peace of mind. We are excited to help our customers achieve that peace of mind when they don’t have enough time in their busy lives to completely clean by themselves.</AboutInfo>
+      <AboutInfo>We are both vegan, and in that lifestyle, we do our absolute best to do all that we can to take care of the planet and its inhabitants.  At home, we use products that are made from clean ingredients, are ethically sourced, and very sustainable. It helps us to have a clear conscience in that, even with the small steps we are taking, it can help lead to big impacts. We hope that by offering cleaning services using products that are both good for your space and also the planet, we can help give you that good feeling of taking small steps.</AboutInfo>
+      <AboutInfo>Clean & Green is a cleaning service where our main focus is to help give our customers a clear conscience. Our goal is to provide that through our eco-friendly cleaning services. We hope to be helping you achieve that peace of mind soon. </AboutInfo>
+    </AboutCard>
   </AboutWrap>
 )
 
--- client/src/components/about/AboutElements.js
+++ client/src/components/about/AboutElements.js
@@ -1,6 +1,38 @@
 import styled from 'styled-components'
 
 export const AboutWrap = styled.div`
-  height: 78vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   background-color: #EBEBEB;
+`
+
+export const AboutTitle = styled.h1`
+  font-family: 'Oswald', sans-serif;
+  font-size: 60px;
+  padding: 40px 0px;
+
+  background: linear-gradient(to right, #60BC46, #019DD0);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`
+
+export const AboutCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 800px;
+  background-color: lightgray;
+  border-radius: 20px;
+  border: 2px solid #000;
+  padding: 30px 0;
+  margin-bottom: 20px;
+`
+
+export const AboutInfo = styled.p`
+  width: 600px;
+  font-family: 'Montserrat', sans-serif;
+  text-indent: 40px;
+  line-height: 25px;
+  font-size: 15px;
 `
